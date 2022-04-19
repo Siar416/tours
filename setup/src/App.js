ff36f57bc227f63cf92742aa1acf6fde3adba6d2
Siar Wahidi <siar.wahidi@gmail.com>
2022-04-19 13:14:35 -0400

    added function to grab data from api. Added try/catch block for api call and set state for loading and tours

--- setup/src/App.js
+++ setup/src/App.js
@@ -11,7 +11,21 @@ function App() {
 
   const fetchTours = async () => {
     setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
   };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
 
   if (loading) {
     return (
